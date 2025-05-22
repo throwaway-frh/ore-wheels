b41a44a1dabbc47394e5052b75f5cc5edc3c54d6
farah.khashman <farah.khashman@acadia.inc>
2025-05-22 14:32:53 +0100

    check unpacling dir

--- unpack_ore_linux.sh
+++ unpack_ore_linux.sh
@@ -10,11 +10,16 @@ echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 # Unpack ORE & ORE-SWIG
 tar xfz ore.tgz
 ls
+echo "@@@@@@@@@@@@@@@@@@@ END ORE-WHEELS @@@@@@@@@@@@@@@@@@@@@@@@"
 cd ore
 ls
+echo "@@@@@@@@@@@@@@@@@@@ END ORE @@@@@@@@@@@@@@@@@@@@@@@@"
 cd ore-swig
+echo "@@@@@@@@@@@@@@@@@@@ END ORE-SWIG @@@@@@@@@@@@@@@@@@@@@@@@"
+cd ..
+cd ..
 #cp exchangerate.hpp ore/QuantLib/ql
 #tar xfz oreswig.tgz
 #cp setup.py oreswig/OREAnalytics-SWIG/Python/setup.py
-cp oreanalytics-config.linux ore/ore-swig/oreanalytics-config
+cp oreanalytics-config.linux ore/ORE-SWIG/oreanalytics-config
 
